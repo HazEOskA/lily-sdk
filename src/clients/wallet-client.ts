4aa4efffa9efa9cfe67b2b41f3f4e8694965e39e
David-patrick-chuks <pd3072894@gmail.com>
2026-05-16 14:10:18 +0100

    feat(wallets): add wallet client module

---
+++ src/clients/wallet-client.ts
@@ -0,0 +1,24 @@
+import type {
+  ProvisionWalletRequest,
+  Wallet,
+  WalletProvisioningResult,
+} from '../models';
+import type { WalletClientContract } from '../types/contracts';
+import { BaseClient } from './base-client';
+
+export class WalletClient extends BaseClient implements WalletClientContract {
+  public provision(input: ProvisionWalletRequest): Promise<WalletProvisioningResult> {
+    return this.request({
+      method: 'POST',
+      path: '/v1/wallets/provision',
+      body: input,
+    });
+  }
+
+  public get(walletId: string): Promise<Wallet> {
+    return this.request({
+      method: 'GET',
+      path: `/v1/wallets/${walletId}`,
+    });
+  }
+}
