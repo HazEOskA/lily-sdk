8d4201c5ffb913ceec6c787a5d3b3d8f7654d1bf
David-patrick-chuks <pd3072894@gmail.com>
2026-05-16 14:10:18 +0100

    feat(payments): add payment client module

---
+++ src/clients/payment-client.ts
@@ -0,0 +1,33 @@
+import type {
+  ExecutePaymentRequest,
+  Payment,
+  PaymentQuote,
+  PaymentQuoteRequest,
+} from '../models';
+import type { PaymentClientContract } from '../types/contracts';
+import { BaseClient } from './base-client';
+
+export class PaymentClient extends BaseClient implements PaymentClientContract {
+  public quote(input: PaymentQuoteRequest): Promise<PaymentQuote> {
+    return this.request({
+      method: 'POST',
+      path: '/v1/payments/quote',
+      body: input,
+    });
+  }
+
+  public execute(input: ExecutePaymentRequest): Promise<Payment> {
+    return this.request({
+      method: 'POST',
+      path: '/v1/payments',
+      body: input,
+    });
+  }
+
+  public get(paymentId: string): Promise<Payment> {
+    return this.request({
+      method: 'GET',
+      path: `/v1/payments/${paymentId}`,
+    });
+  }
+}
